9488f46e1f85b33efabc0bda1cb602ea77674bb6
Arbaz <arbaz582021@gmail.com>
2023-11-19 20:39:51 +0500

    add

--- tree/inseration.cpp
+++ tree/inseration.cpp
@@ -7,6 +7,7 @@ public:
     int data;
     Node *right;
     Node *left;
+
     Node(int data)
     {
         this->data = data;
@@ -18,9 +19,11 @@ class tree
 {
 public:
     Node *root;
+    int count;
     tree()
     {
         root = nullptr;
+        count = 0;
     }
     void insert(int data)
     {
@@ -63,9 +66,9 @@ public:
         {
             return true;
         }
-        bool left = searchInBst(head->left,data);
-        bool right = searchInBst(head->right,data);
-        return left||right;
+        bool left = searchInBst(head->left, data);
+        bool right = searchInBst(head->right, data);
+        return left || right;
     }
     void print(Node *head)
     {
@@ -78,18 +81,91 @@ public:
         print(head->right);
         return;
     }
-    Node* start()
-    {return root;}
+    Node *start()
+    {
+        return root;
+    }
+
+    void Even_Counts(Node *head)
+    {
+        if (!head)
+        {
+            return;
+        }
+        if (head->data % 2 == 0)
+        {
+            count++;
+        }
+        Even_Counts(head->left);
+        Even_Counts(head->right);
+
+        return;
+    }
+    int heightOfBST(Node *head)
+    {
+        int ans = helper(head, 0);
+        return ans - 1;
+    }
+    int helper(Node *temp, int count)
+    {
+        if (!temp)
+        {
+            return 0;
+        }
+        int left = helper(temp->left, count);
+        int right = helper(temp->right, count);
+        return 1+max(left,right);
+    }
+    void sort(){
+        
+    }
 };
 
 int main()
 {
     tree t;
+    t.insert(6);
     t.insert(5);
     t.insert(3);
-    t.insert(1);
-    t.insert(2);
-    t.insert(9);
+    t.insert(4);
+    cout << t.heightOfBST(t.root) << endl;
+    t.Even_Counts(t.root);
+    cout << t.count << "Even number ";
     t.print(t.root);
-    cout<<t.searchInBst(t.root,1);
+    cout << t.searchInBst(t.root, 1);
 }
+
+
+//https://leetcode.com/problems/validate-binary-search-tree/
+
+/*
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if(!root){
+            return true;
+        }
+        if(root->left){
+          if(root->val<root->left->val){
+                return false;
+            }
+            if(root->val==root->left->val){
+                return false;
+            }
+        }
+        if(root->right)
+        {
+            if(root->val>root->right->val){
+                return false;
+            }
+             if(root->val==root->right->val){
+                return false;
+            }
+        }
+        bool left = isValidBST(root->left);
+        bool right = isValidBST(root->right);
+            
+        
+        return left && right;
+    }
+};*/
